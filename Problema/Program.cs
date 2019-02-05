ff51cb1f1612ad587bd5df019d58183a8c8faa3a
Roberto Deresu <roberto.deresu@gmail.com>
2019-02-05 22:22:41 +0200

    Add project files.

---
+++ Problema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
